74b38aea91ba323e99d5708a33a1062363dfb915
Migi <miguelaguilarfere@gmail.com>
2025-11-21 11:02:35 -0600

    Recuperacion de constraseña por Gmail

---
+++ src/config.py
@@ -0,0 +1,15 @@
+class Config:
+    SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'carniceria.molino.la.poblanita@gmail.com'
+    MAIL_PASSWORD = 'hrqelvfxvcdjvhkw'
+    MAIL_DEFAULT_SENDER = 'carniceria.molino.la.poblanita@gmail.com'
+
+class DesarrolloConfig():
+    DEBUG = False
+
+config = {
+    'desarrollador': DesarrolloConfig
+}
